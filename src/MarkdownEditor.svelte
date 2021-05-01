d0288fdef2a0b1419221704390b14fc9e76a42bb
David Moreau <dmoreau@lamobilery.fr>
2021-05-01 17:07:33 +0200

    Added panes skeleton

--- src/MarkdownEditor.svelte
+++ src/MarkdownEditor.svelte
@@ -1,5 +1,17 @@
+<script>
+  import MarkdownText from "./MarkdownText.svelte";
+  import MarkdownPreview from "./MarkdownPreview.svelte";
+</script>
+
 <main>
-  <div class="editor">Ici l'éditeur</div>
+  <div class="editor">
+    <div class="pane">
+      <MarkdownText />
+    </div>
+    <div class="pane">
+      <MarkdownPreview />
+    </div>
+  </div>
 </main>
 
 <style>
@@ -10,10 +22,20 @@
   }
 
   .editor {
+    display: flex;
     margin-top: 8%;
     width: 65%;
     min-height: 50rem;
     box-shadow: 3px 5px 17px -2px rgba(0, 0, 0, 0.81);
     border-radius: 15px;
+  }
+
+  .pane {
+    flex: 50%;
+    padding: 4%;
+  }
+
+  .pane:last-child {
+    border-left: 2px solid black;
   }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import MarkdownEditor from "./MarkdownEditor.svelte";
 
 const markdownEditor = new MarkdownEditor({
   target: document.body,
-  props: {
-    name: "world",
-  },
 });
 
 export default markdownEditor;
